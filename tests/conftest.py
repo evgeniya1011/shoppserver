b357877590e473adf79e42b9e9e2505f4b8d3739
evgeniya1011 <zhdavydova@mail.ru>
2024-01-08 01:13:11 +0300

    add test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,48 +1,29 @@
-import os
-import sys
-import asyncio
 import pytest
-from asgi_lifespan import LifespanManager
-from fastapi import FastAPI
 from fastapi.testclient import TestClient
-from httpx import AsyncClient
 from tortoise.contrib.fastapi import register_tortoise
 
-from main import connect_database
-
-# app = FastAPI()
-#
-#
-# def initialize_tests():
-#     register_tortoise(
-#         app,
-#         db_url='postgres://postgres:azuhin56@localhost:5432/shop_test',
-#         modules={'models': ['app.models']},
-#         generate_schemas=True,
-#         add_exception_handlers=True,
-#     )
+import tortoise_config
+from main import app
 
 
-# создание TestClient после инициализации Tortoise
-# @pytest.fixture
-# def client():
-#     with TestClient(app) as c:
-#         yield c
-#
-#
-# # Фикстура для authenticated_client
-# @pytest.fixture
-# def authenticated_client():
-#     with TestClient(app) as client:
-#         yield client
+def connect_database_test():
+    register_tortoise(
+        app,
+        # db_url="postgres://DB_USERNAME:DB_PASSWORD@localhost:5432/DB_NAME",
+        config=tortoise_config.TORTOISE_ORM_TEST,
+        generate_schemas=True,
+    )
 
-# @pytest.fixture(scope="module")
-# def anyio_backend():
-#     return "asyncio"
-#
-#
-# @pytest.fixture(scope="module")
-# async def client():
-#     async with LifespanManager(app):
-#         async with AsyncClient(app=app, base_url="http://test") as c:
-#             yield c
+
+# Фикстура создания TestClient после инициализации Tortoise
+@pytest.fixture
+def client():
+    with TestClient(app) as c:
+        yield c
+
+
+# Фикстура для authenticated_client
+@pytest.fixture
+def authenticated_client():
+    with TestClient(app) as client:
+        yield client
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,85 +1,60 @@
-import asyncio
-
-import anyio
 import pytest
-from httpx import AsyncClient
-from fastapi.testclient import TestClient
-from tortoise import Tortoise
+from passlib.context import CryptContext
+from main import app
+from tests.conftest import connect_database_test
 
-from app.models import User
-from main import connect_database, app
-
-ANYIO_FORCE_BACKEND = asyncio
-async def init_db():
-    # инициализация базы данных перед тестами
-    await Tortoise.init(
-        db_url='postgres://postgres:azuhin56@localhost:5432/shop_test',
-        modules={"models": ["app.models"]},
-    )
-    await Tortoise.generate_schemas()
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
-async def cleanup_db():
-    # очистка базы данных после тестов
-    await Tortoise.close_connections()
+# Обработчики событий запуска и завершения
+def on_startup():
+    connect_database_test()
 
 
-@pytest.fixture(autouse=True)
-async def setup_and_teardown():
-    await init_db()
-    yield
-    await cleanup_db()
+def on_shutdown():
+    pass
 
 
-@pytest.mark.anyio
-async def test_register_user():
+# Привязка обработчиков событий к приложению
+app.add_event_handler("startup", on_startup)
+app.add_event_handler("shutdown", on_shutdown)
+
+
+@pytest.mark.asyncio
+async def test_register_user(authenticated_client):
     user_test = {
-            "username": "Test Testov",
-            "email": "test@example.com",
-            "phone_number": "+71234567890",
-            "password": "Test123$",
-            "password_confirmation": "Test123$",
+        "username": "Test Testov",
+        "email": "test3@mail.com",
+        "phone_number": "+79354571268",
+        "password": "Test123$",
+        "password_confirmation": "Test123$",
 
-        }
-    async with AsyncClient(app=app, base_url="http://test") as client:
-        # Отправка запроса на регистрацию
-        response = await client.post("/auth/register", json=user_test)
+    }
+    hashed_password = pwd_context.hash(user_test["password"])
+    user_test["password"] = hashed_password
+    user_test["password_confirmation"] = hashed_password
 
-        # Проверка успешного ответа и наличия созданного пользователя в базе данных
-        assert response.status_code == 200
-        assert response.json()["username"] == user_test["username"]
-
-        # # Проверка, что пароль сохранен в хешированном виде
-        user_id = response.json()["id"]
-        registered_user = await User.get(id=user_id)
-        assert registered_user.id == user_id
-        # Очистка данных после теста
-        await User.filter(id=user_id).delete()
-    await cleanup_db()
-
-#
-# @pytest.fixture(autouse=True)
-# async def setup_and_teardown():
-#     await init_db()
-#     yield
-#     await cleanup_db()
+    response = authenticated_client.post(
+        "/auth/register",
+        json=user_test
+    )
+    assert response.status_code == 200
 
 
-# async def main():
-#     await init_db()
-#     await test_register_user()
-#     await cleanup_db()
-#
-# anyio.run(main)
+@pytest.mark.asyncio
+async def test_get_products_unauthorized(client):
+    response = client.get("/products/products")
+    assert response.status_code == 401
 
-# @pytest.mark.anyio
-# async def test_create_user(client: AsyncClient):  # nosec
-#     response = await client.post("/users", json={"username": "admin"})
-#     assert response.status_code == 200, response.text
-#     data = response.json()
-#     assert data["username"] == "admin"
-#     assert "id" in data
-#     user_id = data["id"]
-#
-#     user_obj = await Users.get(id=user_id)
-#     assert user_obj.id == user_id
+
+@pytest.mark.asyncio
+async def test_create_product(client):
+    product_test = {
+        "name": "Фитнес-браслет",
+        "price": 5000
+    }
+    response = client.post(
+        "/products/create-product",
+        json=product_test
+    )
+    assert response.status_code == 200
