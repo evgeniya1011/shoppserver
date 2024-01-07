5986f2c9949018003d17fafd7814a4ab55dd2d62
evgeniya1011 <zhdavydova@mail.ru>
2024-01-08 01:11:38 +0300

    fix 1

--- main.py
+++ main.py
@@ -1,45 +1,16 @@
-import os
-
 from fastapi import FastAPI
 from tortoise.contrib.fastapi import register_tortoise
 
 import tortoise_config
 from app.routers.auth import auth_router
 from app.routers.products import product_router
-from tortoise_config import DATABASE_URL
 
 app = FastAPI()
-# DB_USERNAME = os.getenv('DB_USERNAME')
-# DB_PASSWORD = os.getenv('DB_PASSWORD')
-# DB_HOST = os.getenv('DB_HOST')
-# DB_NAME = os.getenv('DB_NAME')
-
-
-#запуск:
-# register_tortoise(
-#     app,
-#         # db_url="postgres://DB_USERNAME:DB_PASSWORD@localhost:5432/DB_NAME",
-#     db_url=DATABASE_URL,
-#     modules={"models": ["app.models"]},
-#     generate_schemas=True,
-#     add_exception_handlers=True,
-# )
-
-
-# @app.on_event("startup")
-# async def startup_db_client():
-#     await init()
-#
-#
-# @app.on_event("shutdown")
-# async def shutdown_db_client():
-#     await close()
 
 
 def connect_database():
     register_tortoise(
         app,
-        # db_url="postgres://DB_USERNAME:DB_PASSWORD@localhost:5432/DB_NAME",
         config=tortoise_config.TORTOISE_ORM,
         generate_schemas=True,
     )
@@ -52,4 +23,3 @@ if __name__ == "__main__":
     import uvicorn
     connect_database()
     uvicorn.run(app, host="127.0.0.1", port=8000)
-
