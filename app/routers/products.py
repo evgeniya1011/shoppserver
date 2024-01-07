70ade6543f69c562dd05189e835ea8494401aeba
evgeniya1011 <zhdavydova@mail.ru>
2024-01-08 01:12:35 +0300

    fix flake8

--- app/routers/products.py
+++ app/routers/products.py
@@ -7,14 +7,15 @@ product_router = APIRouter()
 
 
 @product_router.get("/products", response_model=list[ProductPydantic])
-async def get_products(page: int = 1, page_size: int = 10, current_user=Depends(get_current_user)):
+async def get_products(page: int = 1, page_size: int = 10,
+                       current_user=Depends(get_current_user)):
+    """Вывод список продуктов"""
     products = await Product.filter(is_active=True).offset(page).limit(page_size).all()
     return products
 
 
 @product_router.post("/create-product", response_model=ProductPydantic)
 async def create_product(product: ProductCreate):
+    """Создание продукта"""
     new_product = await Product.create(name=product.name, price=product.price)
     return await ProductPydantic.from_tortoise_orm(new_product)
-
-
