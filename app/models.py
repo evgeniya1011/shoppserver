184993904237d9da53f34a95399bc4b1b026a5f7
evgeniya1011 <zhdavydova@mail.ru>
2024-01-08 01:12:15 +0300

    fix validator

--- app/models.py
+++ app/models.py
@@ -1,7 +1,4 @@
-from pydantic import BaseModel, EmailStr, field_validator
 from tortoise import fields, models
-import re
-
 from tortoise.contrib.pydantic import pydantic_model_creator
 
 
@@ -25,39 +22,6 @@ class User(models.Model):
         return UserPydantic
 
 
-# class UserRegistration(BaseModel):
-#     username: str
-#     email: EmailStr
-#     phone_number: str
-#     password: str
-#     password_confirmation: str
-#
-#     @field_validator('phone_number')
-#     def validate_phone_number(cls, value):
-#         if not re.match(r'^\+7\d{10}$', value):
-#             raise ValueError('Неверный формат номера телефона')
-#         return value
-#
-#     @field_validator('password')
-#     def validate_password(cls, value):
-#         if len(value) < 8 or not any(char.isupper() for char in value) or not any(char in '$%&!:' for char in value):
-#             raise ValueError('Неверный формат пароля')
-#         return value
-#
-#     @field_validator('password_confirmation')
-#     def validate_password_confirmation(cls, value, **values):
-#         print(values)
-#         if "password" in values and value != values["password"]:
-#             raise ValueError("Пароли не совпадают")
-#         return value
-#
-#
-# class UserLogin(BaseModel):
-#     email_phone_number: str
-#     password: str
-
-
-
 class Product(models.Model):
     """Модель товара"""
 
@@ -71,21 +35,14 @@ class Product(models.Model):
     def __str__(self):
         return f'{self.name} - {self.price}'
 
-
     def __get_pydantic_model__(self):
         return ProductPydantic
 
 
-# class ProductCreate(BaseModel):
-#     name: str
-#     price: int
-
-
 UserPydantic = pydantic_model_creator(User, name='User')
-UserIn_Pydantic = pydantic_model_creator(User, name='UserIn', exclude_readonly=True)
+UserIn_Pydantic = pydantic_model_creator(User, name='UserIn',
+                                         exclude_readonly=True)
 
 ProductPydantic = pydantic_model_creator(Product, name='Product')
-ProductIn_Pydantic = pydantic_model_creator(Product, name='ProductIn', exclude_readonly=True)
-
-
-
+ProductIn_Pydantic = pydantic_model_creator(Product, name='ProductIn',
+                                            exclude_readonly=True)
